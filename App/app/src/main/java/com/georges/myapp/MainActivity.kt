3eef1d10cce53ae995463287fc8a1998411c4336
georgesroberto <georgesroberto21@gmail.com>
2024-03-14 09:13:24 +0300

    Working with Images

--- App/app/src/main/java/com/georges/myapp/MainActivity.kt
+++ App/app/src/main/java/com/georges/myapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.georges.myapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,7 +45,7 @@ class MainActivity : ComponentActivity() {
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(6.dp)
+        modifier = modifier
     ) {
         Text(
             text = message,
@@ -53,19 +58,41 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(
+                    alignment = Alignment.CenterHorizontally
+                )
         )
     }
 }
 
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    Box {
+        val bgImage = painterResource(R.drawable.bgimg)
+        Image(
+            painter = bgImage,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.8F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterEnd)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyAppTheme {
-        GreetingText(
-            message =  "Happy Birthday Janja",
-            from = "From Georges",
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.from_text),
             modifier = Modifier.padding(6.dp)
         )
     }
