0b84b4d6883686c46206847a7853a0ebf53628d1
georgesroberto <georgesroberto21@gmail.com>
2024-03-22 10:42:37 +0300

    Updated Task 2

--- 03 - Display Lists and use Material Design/01 - Fundamentals/Task2 .kt
+++ 03 - Display Lists and use Material Design/01 - Fundamentals/Task2 .kt
@@ -2,7 +2,7 @@ enum class Daypart {
     MORNING, AFTERNOON, EVENING
 } 
 
-data class Event(
+data class NEvent(
     val title: String, 
     val description: String? = null, 
     val daypart: Daypart, 
@@ -10,14 +10,14 @@ data class Event(
 )
 
 fun main(){
-    val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
-    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
-    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
-    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
-    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
-    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+    val event1 = NEvent(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+    val event2 = NEvent(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+    val event3 = NEvent(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+    val event4 = NEvent(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    val event5 = NEvent(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+    val event6 = NEvent(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
 
-	val event = mutableListOf<Event>(event1, event2, event3, event4, event5, event6)
+	val event = mutableListOf<NEvent>(event1, event2, event3, event4, event5, event6)
 
     val groupedEvents = event.groupBy{it.daypart}
     
